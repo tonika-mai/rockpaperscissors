3e27c96126754b20447a54548e6ccc94bbc2a309
tonika-mai <monicaparvanova@gmail.com>
2020-09-14 18:18:10 +0200

    Updates

---
+++ precache-manifest.8bf00af7077b4690d1fb882b1044ec8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54300d339b27439ddbe1b59c5b0ea78c",
+    "url": "/rockpaperscissors/index.html"
+  },
+  {
+    "revision": "8e08bc819cba5b69afaa",
+    "url": "/rockpaperscissors/static/css/main.9bd959c2.chunk.css"
+  },
+  {
+    "revision": "d4ce16f4bd5486e41997",
+    "url": "/rockpaperscissors/static/js/2.81689917.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rockpaperscissors/static/js/2.81689917.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e08bc819cba5b69afaa",
+    "url": "/rockpaperscissors/static/js/main.c8788424.chunk.js"
+  },
+  {
+    "revision": "83c611acba194941da3a",
+    "url": "/rockpaperscissors/static/js/runtime-main.e1124620.js"
+  },
+  {
+    "revision": "538245cb3dd76d6c26c2e1b3d991c989",
+    "url": "/rockpaperscissors/static/media/confetti3.538245cb.jpg"
+  }
+]);
